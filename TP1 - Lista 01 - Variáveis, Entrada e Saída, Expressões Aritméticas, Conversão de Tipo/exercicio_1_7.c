ca0bebe0d60f498091aaa1b5ad7ad9204b2e5616
miltinsalgado <miltonsalgadoleandro@gmail.com>
2022-06-02 00:41:18 -0300

    Update Commit

--- TP1 - Lista 01 - Variáveis, Entrada e Saída, Expressões Aritméticas, Conversão de Tipo/exercicio_1_7.c
+++ TP1 - Lista 01 - Variáveis, Entrada e Saída, Expressões Aritméticas, Conversão de Tipo/exercicio_1_7.c
@@ -5,19 +5,18 @@
 
 #include <stdio.h>
 #include <locale.h>
-#include <math.h>
 
 int main()
 {
     setlocale(LC_ALL, "");
     printf("EXERCÍCIO 1.7\n");
 
-    float raio = 0;
+    float raio = 0, pi = 3.14;
 
     printf("\nDigite o raio da esfera: ");
     scanf("%f", &raio);
     
-    float volume = (float) 4 / 3 * M_PI * pow(raio, 3);
+    float volume = (float) 4 / 3 * pi * raio * raio * raio;
 
     printf("\nVolume da esfera de raio %.2f: %.2f", raio, volume);
 
